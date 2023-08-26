9ed4a33a2303f794fc621433d7707e44c563c305
Sayan Datta <136943338+SayanDatta2004@users.noreply.github.com>
2023-08-26 21:07:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "07/27/",
+    dayMonth = "08/27/",
     birthday = dayMonth + yyyy;
     today = mm + "/" + dd + "/" + yyyy;
     if (today > birthday) {
